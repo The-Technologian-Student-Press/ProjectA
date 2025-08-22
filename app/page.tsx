87c05d737c17b2d275710747df9f58dec3498233
tr-ggr <tristanjamestolentino56@gmail.com>
2025-08-23 00:20:49 +0800

    Feature: add global styles and TRPC setup
    - Introduced global CSS styles with Tailwind and custom properties for theming.
    - Implemented TRPC client and server setup for API communication.
    - Created a query client with hydration support for server-side rendering.
    - Defined a basic app router with a sample query.
    - Configured TypeScript settings for improved development experience.

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+
+export default function Page() {
+  return (
+    <h1>
+      <Button>Hello, Next.js!</Button>
+    </h1>
+  );
+}
