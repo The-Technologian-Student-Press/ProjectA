d165133308d37fad5a8cede8329fcb69b2dc02d0
wetooa <derpykidyt@gmail.com>
2025-09-19 18:35:15 +0800

    Fix: Add other urls in getUrl tRPC

--- trpc/client.tsx
+++ trpc/client.tsx
@@ -21,6 +21,8 @@ function getQueryClient() {
 function getUrl() {
   const base = (() => {
     if (typeof window !== "undefined") return "";
+    if (process.env.NODE_ENV === "development") return "http://localhost:3000";
+    if (process.env.STAGING_URL) return `${process.env.STAGING_URL}`;
     if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
     return "http://localhost:3000";
   })();
